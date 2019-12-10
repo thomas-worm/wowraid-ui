5ccd889f00079ec6ff0da91f2cdca87881bb6575
thomas-worm <thomas.worm@thomas-worm.de>
2019-12-10 13:01:18 +0000

    Added form group for create character form.

--- src/app/user/character/character.component.ts
+++ src/app/user/character/character.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-character',
@@ -9,38 +7,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CharacterComponent implements OnInit {
 
-  realms: String[] = [];
-  factions: String[] = [];
-
-  character = new FormGroup({
-    realm: new FormControl({value: null, disabled: true}),
-    characterName: new FormControl({value: '', disabled: false}),
-    faction: new FormControl({value: null, disabled: true}),
-    race: new FormControl({value: null, disabled: true}),
-    class: new FormControl({value: null, disabled: true})
-  });
-
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
-    this.http.get<String[]>('https://wowraid-api.herokuapp.com/realm', {withCredentials: true}).subscribe(realms => {
-      this.realms = realms;
-      this.character.get('realm').enable();
-    });
-    this.http.get<String[]>('https://wowraid-api.herokuapp.com/faction', {withCredentials: true}).subscribe(factions => {
-      this.factions = factions;
-      this.character.get('faction').enable();
-    });
-  }
-
-  translateFaction(faction: string): string {
-    if (faction.toLocaleLowerCase() == 'alliance') {
-      return 'Allianz';
-    }
-    if (faction.toLocaleLowerCase() == 'horde') {
-      return 'Horde';
-    }
-    return faction;
   }
 
 }
--- src/app/user/character/create/create.component.ts
+++ src/app/user/character/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterCreateComponent implements OnInit {
 
-  constructor() { }
+  realms: String[] = [];
+  factions: String[] = [];
+
+  character = new FormGroup({
+    realm: new FormControl({value: null, disabled: true}),
+    characterName: new FormControl({value: '', disabled: false}),
+    faction: new FormControl({value: null, disabled: true}),
+    race: new FormControl({value: null, disabled: true}),
+    class: new FormControl({value: null, disabled: true})
+  });
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<String[]>('https://wowraid-api.herokuapp.com/realm', {withCredentials: true}).subscribe(realms => {
+      this.realms = realms;
+      this.character.get('realm').enable();
+    });
+    this.http.get<String[]>('https://wowraid-api.herokuapp.com/faction', {withCredentials: true}).subscribe(factions => {
+      this.factions = factions;
+      this.character.get('faction').enable();
+    });
+  }
+
+  translateFaction(faction: string): string {
+    if (faction.toLocaleLowerCase() == 'alliance') {
+      return 'Allianz';
+    }
+    if (faction.toLocaleLowerCase() == 'horde') {
+      return 'Horde';
+    }
+    return faction;
   }
 
 }
