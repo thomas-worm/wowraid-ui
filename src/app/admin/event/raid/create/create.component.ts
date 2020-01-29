f2b6e0026388a2278a5c8c486d32a6662f1e8d8a
thomas-worm <thomas.worm@thomas-worm.de>
2020-01-29 21:11:49 +0000

    Added components for raid management.

--- src/app/admin/event/raid/create/create.component.ts
+++ src/app/admin/event/raid/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -7,9 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRaidEventComponent implements OnInit {
 
-  constructor() { }
+  raid: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.raid = this.formBuilder.group({
+      'key': '',
+      'name': '',
+      'start_date_time': '',
+      'finish_date_time': '',
+      'instances': this.formBuilder.array([])
+    });
+  }
+
+  createInstanceFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      'key': '',
+      'name': '',
+      'start_date_time': '',
+      'finish_date_time': '',
+      'bosses': this.formBuilder.array([])
+    });
+  }
+
+  createBossFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      'key': '',
+      'name': '',
+      'start_date_time': '',
+      'finish_date_time': ''
+    });
+  }
+
+  addInstanceFormGroup() {
+    (this.raid.get('instances') as FormArray).push(this.createInstanceFormGroup());
+  }
+
+  addBossFormGroup(instance: number) {
+    ((this.raid.get('instances') as FormArray).controls[instance].get('bosses') as FormArray).push(this.createBossFormGroup());
   }
 
 }
