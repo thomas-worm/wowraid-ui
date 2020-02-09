dca236340b9e298df8266b293a65a30a4e0bfa4a
thomas-worm <thomas.worm@thomas-worm.de>
2020-02-09 16:48:39 +0000

    Added popovers to transactions.

--- src/app/epgp/account/transaction/transaction.component.ts
+++ src/app/epgp/account/transaction/transaction.component.ts
@@ -11,11 +11,30 @@ import { formatDate } from '@angular/common';
 export class EpGpAccountTransactionComponent implements OnInit {
 
   @Input() transaction: EpGpTransaction;
-  popoverContent: string = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  popoverContent(transaction: EpGpTransaction): string {
+    var popoverContent: string = '';
+    popoverContent += '<dl class="row">';
+    popoverContent += '<dt class="col">Titel:</dt><dd class="row">' + escape(transaction.title) + '</dd>';
+    popoverContent += '<dt class="col">Zeitpunkt:</dt><dd class="row">' + escape(formatDate(transaction.date_time, 'dd.MM.yyyy, HH:mm \'Uhr\'', 'de')) + '</dd>';
+    popoverContent += '<dt class="col">Wert:</dt><dd class="row">' + escape(transaction.value) + ' ' + escape(transaction.currency) + '</dd>';
+    if (transaction.description != null && transaction.description.trim() != '') {
+      popoverContent += '<dt class="col">Beschreibung:</dt><dd class="row">' + escape(transaction.description) + '</dd>';
+    }
+    if (transaction.events != null && transaction.events.length > 0) {
+      popoverContent += '<dt class="col">Ereignisse:</dt><dd class="row"><';
+      transaction.events.forEach(event => {
+        popoverContent += '<li>' + escape(event.name) + '</li>';
+      });
+      popoverContent += '</dd>';
+    }
+    popoverContent += '</dl>';
+    return popoverContent;
   }
 
   /** @Input()
