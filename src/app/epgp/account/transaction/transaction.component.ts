58f8f725f81277d9fa63e2d1aa8f8843046eedd9
thomas-worm <thomas.worm@thomas-worm.de>
2020-02-09 17:35:03 +0000

    Added popovers to transactions.

--- src/app/epgp/account/transaction/transaction.component.ts
+++ src/app/epgp/account/transaction/transaction.component.ts
@@ -20,18 +20,18 @@ export class EpGpAccountTransactionComponent implements OnInit {
   popoverContent(transaction: EpGpTransaction): string {
     var popoverContent: string = '';
     popoverContent += '<dl class="row">';
-    popoverContent += '<dt class="col">Titel:</dt><dd class="row">' + _.escape(transaction.title) + '</dd>';
-    popoverContent += '<dt class="col">Zeitpunkt:</dt><dd class="row">' + _.escape(formatDate(transaction.date_time, 'dd.MM.yyyy, HH:mm \'Uhr\'', 'en-US')) + '</dd>';
-    popoverContent += '<dt class="col">Wert:</dt><dd class="row">' + _.escape(transaction.value) + ' ' + _.escape(transaction.currency) + '</dd>';
+    popoverContent += '<dt class="col">Titel:</dt><dd class="col">' + _.escape(transaction.title) + '</dd>';
+    popoverContent += '<dt class="col">Zeitpunkt:</dt><dd class="col">' + _.escape(formatDate(transaction.date_time, 'dd.MM.yyyy, HH:mm \'Uhr\'', 'en-US')) + '</dd>';
+    popoverContent += '<dt class="col">Wert:</dt><dd class="col">' + _.escape(transaction.value) + ' ' + _.escape(transaction.currency) + '</dd>';
     if (transaction.description != null && transaction.description.trim() != '') {
-      popoverContent += '<dt class="col">Beschreibung:</dt><dd class="row">' + _.escape(transaction.description) + '</dd>';
+      popoverContent += '<dt class="col">Beschreibung:</dt><dd class="col">' + _.escape(transaction.description) + '</dd>';
     }
     if (transaction.events != null && transaction.events.length > 0) {
-      popoverContent += '<dt class="col">Ereignisse:</dt><dd class="row"><';
+      popoverContent += '<dt class="col">Ereignisse:</dt><dd class="col"><ul>';
       transaction.events.forEach(event => {
         popoverContent += '<li>' + _.escape(event.name) + '</li>';
       });
-      popoverContent += '</dd>';
+      popoverContent += '</ul></dd>';
     }
     popoverContent += '</dl>';
     return popoverContent;
