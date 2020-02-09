ff1e4e98844fa3ed632046ffaaa8903c2540031d
thomas-worm <thomas.worm@thomas-worm.de>
2020-02-09 16:54:07 +0000

    Added popovers to transactions.

--- src/app/epgp/account/transaction/transaction.component.ts
+++ src/app/epgp/account/transaction/transaction.component.ts
@@ -18,8 +18,8 @@ export class EpGpAccountTransactionComponent implements OnInit {
   }
 
   popoverContent(transaction: EpGpTransaction): string {
-    var popoverContent: string = '';
-    popoverContent += '<dl class="row">';
+    var popoverContent: string = 'Test';
+    /** popoverContent += '<dl class="row">';
     popoverContent += '<dt class="col">Titel:</dt><dd class="row">' + escape(transaction.title) + '</dd>';
     popoverContent += '<dt class="col">Zeitpunkt:</dt><dd class="row">' + escape(formatDate(transaction.date_time, 'dd.MM.yyyy, HH:mm \'Uhr\'', 'de')) + '</dd>';
     popoverContent += '<dt class="col">Wert:</dt><dd class="row">' + escape(transaction.value) + ' ' + escape(transaction.currency) + '</dd>';
@@ -33,7 +33,7 @@ export class EpGpAccountTransactionComponent implements OnInit {
       });
       popoverContent += '</dd>';
     }
-    popoverContent += '</dl>';
+    popoverContent += '</dl>'; **/
     return popoverContent;
   }
 
