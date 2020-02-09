e8ac06681e8128158ab8a6884637afc902ba115d
thomas-worm <thomas.worm@thomas-worm.de>
2020-02-09 19:05:58 +0000

    Added popovers to transactions.

--- src/app/epgp/account/transaction/transaction.component.ts
+++ src/app/epgp/account/transaction/transaction.component.ts
@@ -27,14 +27,14 @@ export class EpGpAccountTransactionComponent implements OnInit, AfterViewInit {
   popoverContent(transaction: EpGpTransaction): string {
     var popoverContent: string = '';
     popoverContent += '<dl class="row">';
-    popoverContent += '<dt class="col-3">Titel:</dt><dd class="col-9">' + _.escape(transaction.title) + '</dd>';
-    popoverContent += '<dt class="col-3">Zeitpunkt:</dt><dd class="col-9">' + _.escape(formatDate(transaction.date_time, 'dd.MM.yyyy, HH:mm \'Uhr\'', 'en-US')) + '</dd>';
-    popoverContent += '<dt class="col-3">Wert:</dt><dd class="col-9">' + _.escape(transaction.value) + ' ' + _.escape(transaction.currency) + '</dd>';
+    popoverContent += '<dt class="col-3 text-nowrap">Titel:</dt><dd class="col-9">' + _.escape(transaction.title) + '</dd>';
+    popoverContent += '<dt class="col-3 text-nowrap">Zeitpunkt:</dt><dd class="col-9">' + _.escape(formatDate(transaction.date_time, 'dd.MM.yyyy, HH:mm \'Uhr\'', 'en-US')) + '</dd>';
+    popoverContent += '<dt class="col-3 text-nowrap">Wert:</dt><dd class="col-9">' + _.escape(transaction.value) + ' ' + _.escape(transaction.currency) + '</dd>';
     if (transaction.description != null && transaction.description.trim() != '') {
-      popoverContent += '<dt class="col-3">Beschreibung:</dt><dd class="col-9">' + _.escape(transaction.description) + '</dd>';
+      popoverContent += '<dt class="col-3 text-nowrap">Beschreibung:</dt><dd class="col-9">' + _.escape(transaction.description) + '</dd>';
     }
     if (transaction.events != null && transaction.events.length > 0) {
-      popoverContent += '<dt class="col-3">Ereignisse:</dt><dd class="col-9"><ul class="row">';
+      popoverContent += '<dt class="col-3 text-nowrap">Ereignisse:</dt><dd class="col-9"><ul class="row">';
       transaction.events.forEach(event => {
         popoverContent += '<li>' + _.escape(event.name) + '</li>';
       });
