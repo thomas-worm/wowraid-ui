7fdb6264cdb4ced845364ad8b7aee630e29eb3c0
thomas-worm <thomas.worm@thomas-worm.de>
2020-02-10 14:56:40 +0000

    Added loot admin components component structures and changed locale of EPGP to 'de'.

--- src/app/epgp/account/transaction/transaction.component.ts
+++ src/app/epgp/account/transaction/transaction.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { EpGpTransaction } from 'src/app/model/epgptransaction.model';
-import { formatDate } from '@angular/common';
+import { formatDate, formatNumber } from '@angular/common';
 import _ from 'lodash';
 
 declare var $: any;
@@ -29,7 +29,7 @@ export class EpGpAccountTransactionComponent implements OnInit, AfterViewInit {
     popoverContent += '<dl class="row">';
     popoverContent += '<dt class="col-3 text-nowrap">Titel:</dt><dd class="col-9">' + _.escape(transaction.title) + '</dd>';
     popoverContent += '<dt class="col-3 text-nowrap">Zeitpunkt:</dt><dd class="col-9">' + _.escape(formatDate(transaction.date_time, 'dd.MM.yyyy, HH:mm \'Uhr\'', 'de')) + '</dd>';
-    popoverContent += '<dt class="col-3 text-nowrap">Wert:</dt><dd class="col-9">' + _.escape(transaction.value) + ' ' + _.escape(transaction.currency) + '</dd>';
+    popoverContent += '<dt class="col-3 text-nowrap">Wert:</dt><dd class="col-9">' + _.escape(formatNumber(transaction.value, 'de', '1.2-2')) + ' ' + _.escape(transaction.currency) + '</dd>';
     if (transaction.description != null && transaction.description.trim() != '') {
       popoverContent += '<dt class="col-3 text-nowrap">Beschreibung:</dt><dd class="col-9">' + _.escape(transaction.description) + '</dd>';
     }
